6c4e73654183187258e66b668455a87f40fb1ed8
Milos Sreckovic <milossreckovic37@gmail.com>
2026-07-11 14:34:09 +0200

    change of the place for the classes

---
+++ tests/demo/pw.adv.capabilities.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Login functionality", () => {
+  test.beforeEach("Go to the login page", async ({ page }) => {
+    //launch url and assert title and header
+    await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await expect(page).toHaveTitle("CURA Healthcare Service");
+    await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+
+    //click on make appointment button
+    await page.getByRole("link", { name: "Make Appointment" }).click();
+    await expect(page.getByText("Please login to make")).toBeVisible();
+  });
+
+  test.only("Should login successfully", async ({ page }) => {
+    //successful login with valid credentials
+    await page.getByLabel("Username").fill("John Doe");
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //assert if the text is correct
+    await expect(page.locator("h2")).toContainText("Make Appointment");
+  });
+
+  test("Should prevent login with negative credentials", async ({ page }) => {
+    //Unsuccessful login with invalid credentials
+    await page.getByLabel("Username").fill("John Smith"); //different name to test negative scenario
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //assert an error message is displayed
+    await expect(page.locator("#login")).toContainText(
+      "Login failed! Please ensure the username and password are valid.",
+    );
+  });
+});
