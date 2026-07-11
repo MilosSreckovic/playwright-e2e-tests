92775a163a4ccba4e19d08172bb7ef3839ece19e
Milos Sreckovic <milossreckovic37@gmail.com>
2026-07-11 11:59:50 +0200

    Added some new tests

---
+++ tests/functional/debug.make.apptmnt.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Should make Appointment", () => {
+
+
+  test.beforeEach("Should login successfully with valid credentials",async ({ page }) => { 
+
+      //launch url and assert title and header
+      await page.goto("https://katalon-demo-cura.herokuapp.com/");
+      await expect(page).toHaveTitle("CURA Healthcare Service");
+      await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+
+      //click on make appointment button
+      //imena svakog elementa na tastaturi mozemo naci online
+      await page.getByRole("link", { name: "Make Appointment" }).click();
+      await expect(page.getByText("Please login to make")).toBeVisible();
+
+      //successful login with valid credentials
+      await page.getByLabel("Username").fill("John Doe");
+      await page.getByLabel("Password").fill("ThisIsNotAPassword");
+      await page.getByRole("button", { name: "Login" }).click();
+
+      //assert if the text is correct
+      await expect(page.locator("h2")).toContainText("Make Appointment");
+    },
+  );
+
+  test("Should make an appointment with non default values", async ({ page }) => {
+    //dropdown
+    await page.getByLabel("Facility").selectOption("Seoul CURA Healthcare Center");
+
+    //checkbox
+    await page.getByText("Apply for hospital readmission").click();
+
+    //radiobox
+    await page.getByText("Medicaid").click();
+
+    //date picker
+    await page.locator("span").click();
+    //ovde stavimo neku gresku namerno npr broj veci od 31. i onda pada test
+    await page.getByRole("cell", { name: "11" }).nth(3737).click();
+
+    //multiline text box
+    await page.getByRole("textbox", { name: "Comment" }).click();
+    await page.getByRole("textbox", { name: "Comment" }).fill("This is a codegen practice\nin multiple lines");
+
+    //button
+    await page.getByRole("button", { name: "Book Appointment" }).click();
+
+    //assertio
+    await expect(page.locator("h2")).toContainText("Appointment Confirmation");
+    await expect(page.getByRole("link", { name: "Go to Homepage" }),).toBeVisible();
+  });
+});
